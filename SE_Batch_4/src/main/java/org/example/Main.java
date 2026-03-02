078d1c3aee194826a2e050ab9c990cdef2f100f7
Min Pyae Hein <minpyaehein536h@prestigein.com>
2026-03-02 20:10:25 +0700

    Day 1

--- SE_Batch_4/src/main/java/org/example/Main.java
+++ SE_Batch_4/src/main/java/org/example/Main.java
@@ -4,14 +4,22 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        byte smallInteger=127;
+        short mediumInteger=5000;
+        int standardInteger=10000;
+        long largeInteger=1999999L;
+        float floatingPointNumber=1.1f;
+        double floatingPointNumberDouble=1.1d;
+        char singleCharacter='a';
+        boolean true_false=false;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("smallInteger="+smallInteger
+                          +"\nmediumInteger="+mediumInteger
+                          +"\nstandardInteger="+standardInteger
+                          +"\nlargeInteger="+largeInteger
+                          +"\nfloatingPointNumber="+floatingPointNumber
+                          +"\nfloatingPointNumberDouble="+floatingPointNumberDouble
+                          +"\nsingleCharacter="+singleCharacter
+                          +"\ntrue_false="+true_false);
     }
 }
