de97fd6c9a1675c78475d2d1a8f8c150d1b41ab9
Min Pyae Hein <minpyaehein536h@prestigein.com>
2026-03-12 12:19:16 +0700

    fix Day 4 code

---
+++ SE_Batch_4/src/main/java/org/example/Day3/JavaMethod.java
@@ -0,0 +1,5 @@
+package org.example.Day3;
+
+public class JavaMethod {
+
+}
---
+++ SE_Batch_4/src/main/java/org/example/Day4/Conduction.java
@@ -0,0 +1,32 @@
+package org.example.Day4;
+
+import java.util.Scanner;
+
+public class Conduction {
+    public static void main(String[] args) {
+        //if value is equal they will share the heap address and the same value
+//        String gender="sdfsdfsdf";
+//        String gender2="male";
+
+        //create new value in heap
+        String gender1=new String("male");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your gender");
+        String gender=sc.nextLine();
+
+        if(gender.equalsIgnoreCase( "male")){
+            System.out.print("25% discount ");
+        }else if(gender.equalsIgnoreCase("female")){
+            System.out.print("50% discount ");
+        }else{
+            System.out.print("Please Enter Valid Gender");
+        }
+
+    }
+
+    public static void adding(){
+        int total=3+5;
+        System.out.println("Total is "+total);
+    }
+}
