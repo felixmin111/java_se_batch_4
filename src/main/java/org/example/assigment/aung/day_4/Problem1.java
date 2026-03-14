93800abcbddb5568e5f40da010abe404cb2851e7
aungmyintunsoe <aungmyintunsoe1@gmail.com>
2026-03-14 23:55:01 +0800

    maven initialized

--- src/main/java/org/example/assigment/aung/day_4/Problem1.java
+++ src/main/java/org/example/assigment/aung/day_4/Problem1.java
@@ -1,4 +1,7 @@
+package org.example.assigment.aung.day_4;
 import java.util.Scanner;
+
+
 public class Problem1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
