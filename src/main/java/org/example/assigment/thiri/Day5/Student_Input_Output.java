e0140a5dd839381f98d7bceb9d130ce878f2bc9d
Thiri Moe Theint <theerimoetheint@gmail.com>
2026-03-18 03:32:12 +0800

    Day 5 Assignment Final

--- src/main/java/org/example/assigment/thiri/Day5/Student_Input_Output.java
+++ src/main/java/org/example/assigment/thiri/Day5/Student_Input_Output.java
@@ -1,11 +1,60 @@
 package org.example.assigment.thiri.Day5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Student_Input_Output {
-    public static void main(String[] args) {
-        Student_Registration_System student = new Student_Registration_System("Thiri", "Yangon");
-        student.setAge(10);
-        student.setAmount(100);
-        student.setEmail("theerimoetheint@gmail.com");
-        student.display();
-    }
+      public static void main(String[] args) {
+            askStudentInfo();
+      }
+
+      public static void askStudentInfo() {
+            while (true) {
+                  Scanner sc = new Scanner(System.in);
+
+                  Student_Registration_System student = new Student_Registration_System();
+
+                  System.out.println("---------Fill Student Information---------");
+
+                  System.out.println("Enter student name: ");
+                  student.name = sc.nextLine();
+
+                  System.out.println("Enter student age: ");
+                  int age = sc.nextInt();
+                  student.setAge(age);
+                  sc.nextLine();
+
+                  System.out.println("Enter student address: ");
+                  student.address = sc.nextLine();
+
+                  System.out.println("Enter student amount: ");
+                  double amount = sc.nextDouble();
+                  student.setAmount(amount);
+                  sc.nextLine();
+
+                  System.out.println("Enter student email: ");
+                  String email = sc.nextLine();
+                  student.setEmail(email);
+
+                  student.display();
+
+                  while (true) {
+                        System.out.println("Do you want to put another student (yes or no); ");
+                        String ans = sc.nextLine();
+
+                        if ((ans.equalsIgnoreCase("yes"))) {
+                              break;
+                        } else if (ans.equalsIgnoreCase("no")) {
+                              System.out.println("----------Thank you for registration!-------------");
+                              System.exit(0);
+
+                        } else {
+                              System.out.println("--------------------------------------------------");
+                              System.out.println("               !!!Invalid Input!!!                ");
+                              System.out.println("           Please fill \"yes\" or \"no\"          ");
+                              System.out.println("--------------------------------------------------");
+                        }
+                  }
+            }
+      }
 }
--- src/main/java/org/example/assigment/thiri/Day5/Student_Registration_System.java
+++ src/main/java/org/example/assigment/thiri/Day5/Student_Registration_System.java
@@ -1,6 +1,8 @@
 package org.example.assigment.thiri.Day5;
 
 public class Student_Registration_System {
+
+    //Initialization
     String name;
     private int age;
     String address;
@@ -22,33 +24,29 @@ public class Student_Registration_System {
             this.amount = amount;
         }
     }
-    public double getAmount() {
-        return amount;
-    }
+    public double getAmount() {return amount;}
 
     //setEmail & getEmail
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
+    public String getEmail() {return email;}
 
-    public Student_Registration_System (String name, String address) {
-        System.out.println("Student Registration");
+    //Constructor
+    public Student_Registration_System () {
         studentCount += 1;
-        this.name = name;
-        this.address = address;
     }
 
+    //display output
     public void display() {
             System.out.println("---------Student Registration Confirmation--------");
-            System.out.println("Name: " + this.name);
-            System.out.println("Age: " + this.age);
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
             System.out.println("Address: " + this.address);
             System.out.println("Amount: " + this.amount);
             System.out.println("Email: " + this.email);
+            System.out.println("Student Count: " + studentCount);
+            System.out.println("--------------------------------------------------");
     }
 
 }
