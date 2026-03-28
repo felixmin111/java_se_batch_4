d443d4b30fa6940013484c47d3af46a89ed1258a
ryd3r-cntrlz <lapyaea.30@gmail.com>
2026-03-28 14:59:32 +0800

    star

--- src/main/java/org/example/assigment/lapyae/day6/star.java
+++ src/main/java/org/example/assigment/lapyae/day6/star.java
@@ -4,12 +4,17 @@ public class star {
     public static void main(String[] args) {
         starEx();
         starEx1();
+        starEx2();
+        starEx3();
+        starEx4();
+        starEx5();
     }
+
     public static void starEx() {
-        int count=0;
-        while (6>count){
-            int innerCount=0;
-            while (count>innerCount){
+        int count = 0;
+        while (6 > count) {
+            int innerCount = 0;
+            while (count > innerCount) {
                 System.out.print("* ");
                 innerCount++;
             }
@@ -17,11 +22,12 @@ public class star {
             count++;
         }
     }
+
     public static void starEx1() {
-        int count=0;
-        while (6>count){
-            int innerCount=6;
-            while (count<innerCount){
+        int count = 0;
+        while (6 > count) {
+            int innerCount = 6;
+            while (count < innerCount) {
                 System.out.print("* ");
                 innerCount--;
             }
@@ -29,4 +35,97 @@ public class star {
             count++;
         }
     }
+
+    public static void starEx2() {
+        int count = 1;
+        while (5 >= count) {
+            int spaceCount = 1;
+            while (spaceCount <= 5 - count) {
+                System.out.print(" ");
+                spaceCount++;
+            }
+            int innerCount = 1;
+            while (innerCount <= (2 * count - 1)) {
+                System.out.print("*");
+                innerCount++;
+            }
+            System.out.println();
+            count++;
+        }
+    }
+
+    public static void starEx3() {
+        int count = 5;
+        int n = 5;
+        while (count > 0) {
+
+            int spaceCount = 0;
+            while (spaceCount < (n - count)) {
+                System.out.print(" ");
+                spaceCount++;
+            }
+
+            int innerCount = 0;
+            while (innerCount < (2 * count - 1)) {
+                System.out.print("*");
+                innerCount++;
+            }
+            System.out.println();
+            count--;
+        }
+    }
+    public static void starEx4() {
+        int n = 5;
+
+        System.out.println("* ");
+
+        int count = 2;
+        while (count < n) {
+            System.out.print("* ");
+            int spaceCount = 1;
+            while (spaceCount <= (count - 2)) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+            System.out.println("* ");
+            count++;
+        }
+
+        int baseCount = 1;
+        while (baseCount <= n) {
+            System.out.print("* ");
+            baseCount++;
+        }
+        System.out.println();
+    }
+
+    public static void starEx5() {
+        int n = 5;
+
+        int baseCount = 1;
+        while (baseCount <= n) {
+            System.out.print("* ");
+            baseCount++;
+        }
+        System.out.println();
+
+        int count = n - 1;
+        while (count >= 2) {
+
+            System.out.print("* ");
+
+            int spaceCount = 1;
+            while (spaceCount <= (count - 2)) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+
+            System.out.println("* ");
+            count--;
+        }
+
+        System.out.println("* ");
+    }
+
+
 }
