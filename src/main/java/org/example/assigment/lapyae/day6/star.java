4db8897a48e909618c345588907aa93030cec875
ryd3r-cntrlz <lapyaea.30@gmail.com>
2026-03-30 23:53:10 +0800

    star for

--- src/main/java/org/example/assigment/lapyae/day6/star.java
+++ src/main/java/org/example/assigment/lapyae/day6/star.java
@@ -8,9 +8,14 @@ public class star {
         starEx3();
         starEx4();
         starEx5();
+        starEx6();
+        starEx7();
+        starEx8();
+        starEx9();
     }
 
     public static void starEx() {
+
         int count = 0;
         while (6 > count) {
             int innerCount = 0;
@@ -24,9 +29,10 @@ public class star {
     }
 
     public static void starEx1() {
+        System.out.println("-------------------");
         int count = 0;
         while (6 > count) {
-            int innerCount = 6;
+            int innerCount = 5;
             while (count < innerCount) {
                 System.out.print("* ");
                 innerCount--;
@@ -37,6 +43,7 @@ public class star {
     }
 
     public static void starEx2() {
+        System.out.println("-------------------");
         int count = 1;
         while (5 >= count) {
             int spaceCount = 1;
@@ -55,6 +62,7 @@ public class star {
     }
 
     public static void starEx3() {
+        System.out.println("-------------------");
         int count = 5;
         int n = 5;
         while (count > 0) {
@@ -75,6 +83,7 @@ public class star {
         }
     }
     public static void starEx4() {
+        System.out.println("-------------------");
         int n = 5;
 
         System.out.println("* ");
@@ -100,6 +109,7 @@ public class star {
     }
 
     public static void starEx5() {
+        System.out.println("-------------------");
         int n = 5;
 
         int baseCount = 1;
@@ -127,5 +137,152 @@ public class star {
         System.out.println("* ");
     }
 
+    public static void starEx6() {
+        System.out.println("-------------------");
+        int n = 5;
 
+        int tipSpace = 1;
+        while (tipSpace <= n - 1) {
+            System.out.print("  ");
+            tipSpace++;
+        }
+        System.out.println("* ");
+
+        int count = 2;
+        while (count < n) {
+
+            int spaceCount = 1;
+            while (spaceCount <= n - count) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+
+            System.out.print("* ");
+
+            int internalSpace = 1;
+            while (internalSpace <= (2 * count - 3)) {
+                System.out.print("  ");
+                internalSpace++;
+            }
+
+            System.out.println("* ");
+            count++;
+        }
+
+        int baseCount = 1;
+        while (baseCount <= 9) {
+            System.out.print("* ");
+            baseCount++;
+        }
+        System.out.println();
+    }
+    public static void starEx7() {
+        System.out.println("-------------------");
+        int n = 5;
+
+        int baseCount = 1;
+        while (baseCount <= 9) {
+            System.out.print("* ");
+            baseCount++;
+        }
+        System.out.println();
+
+        int count = n - 1;
+        while (count >= 2) {
+
+            int spaceCount = 1;
+            while (spaceCount <= n - count) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+
+            System.out.print("* ");
+
+
+            int internalSpace = 1;
+            while (internalSpace <= (2 * count - 3)) {
+                System.out.print("  "); // Double space
+                internalSpace++;
+            }
+
+            System.out.println("* ");
+            count--;
+        }
+
+        int tipSpace = 1;
+        while (tipSpace <= n - 1) {
+            System.out.print("  ");
+            tipSpace++;
+        }
+        System.out.println("* ");
+    }
+
+    public static void starEx8(){
+        System.out.println("-------------------");
+        int n = 0;
+        while(n<5){
+            int j = 0;
+            while(j<5) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            n++;
+        }
+    }
+    public static void starEx9() {
+        System.out.println("-------------------");
+        int n = 5;
+
+        int count = 0;
+        while (count < n / 2) {
+
+            int spaceCount = 0;
+            while (spaceCount < count) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+
+            System.out.print("* ");
+
+            int internalSpace = 0;
+            int maxInternal = n - 2 - (2 * count);
+            while (internalSpace < maxInternal) {
+                System.out.print("  ");
+                internalSpace++;
+            }
+
+            System.out.println("* ");
+            count++;
+        }
+
+        int midSpace = 0;
+        while (midSpace < n / 2) {
+            System.out.print("  ");
+            midSpace++;
+        }
+        System.out.println("* ");
+
+        count = n / 2 - 1;
+        while (count >= 0) {
+
+            int spaceCount = 0;
+            while (spaceCount < count) {
+                System.out.print("  ");
+                spaceCount++;
+            }
+
+            System.out.print("* ");
+
+            int internalSpace = 0;
+            int maxInternal = n - 2 - (2 * count);
+            while (internalSpace < maxInternal) {
+                System.out.print("  ");
+                internalSpace++;
+            }
+
+            System.out.println("* ");
+            count--;
+        }
+    }
 }
---
+++ src/main/java/org/example/assigment/lapyae/day6/starFor.java
@@ -0,0 +1,150 @@
+package org.example.assigment.lapyae.day6;
+
+public class starFor {
+    public static void main(String[] args) {
+        starFor1(5);
+        starFor2(5);
+        starFor3(5);
+        starFor4(5);
+        starFor5(5);
+        starFor6(5);
+        starFor7(5);
+        starFor8(5);
+        starFor9(5);
+        starFor10(5);
+    }
+    public static void starFor1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor2(int n) {
+        System.out.println("---------------------");
+        for (int i = 1; i<=n; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor3(int n) {
+        System.out.println("---------------------");
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+
+    public static void starFor4(int n) {
+        System.out.println("---------------------");
+        for (int i = n; i >= 1; i--) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void starFor5(int n) {
+        System.out.println("---------------------");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor6(int n) {
+        System.out.println("---------------------");
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor7(int n) {
+        System.out.println("---------------------");
+        for (int i = 1; i <= n; i++) {
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1) || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor8(int n) {
+        System.out.println("---------------------");
+        for (int i = n; i >= 1; i--) {
+           
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (i == n || j == 1 || j == (2 * i - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor9(int n) {
+        System.out.println("---------------------");
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void starFor10(int n) {
+        System.out.println("---------------------");
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i + j == n + 1)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+}
