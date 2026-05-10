b4296a04c1cca015c2c500d15e980d8f538f3f28
aungmyintunsoe <aungmyintunsoe1@gmail.com>
2026-05-10 17:55:05 +0800

    Super class error

--- src/main/java/org/example/assigment/aung/petassignment/validator/PetValidator.java
+++ src/main/java/org/example/assigment/aung/petassignment/validator/PetValidator.java
@@ -9,45 +9,49 @@ public class PetValidator {
     public static void validate(Object object) {
         Class<?> clazz = object.getClass();
         System.out.println("class name-->" + clazz.getName());
-        Field[] fields = clazz.getDeclaredFields();
         String errorMessages = null;
         StringBuilder br = new StringBuilder();
-        for (Field field : fields) {
-            field.setAccessible(true);
-            System.out.println("field name-->" + field.getName());
-
-            if (field.isAnnotationPresent(EmptyField.class)) {
-                try {
-                    Object value = field.get(object);
-                    if (value == null || value.equals("")) {
-                        EmptyField annotation = field.getAnnotation(EmptyField.class);
+        
+        while (clazz != null) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                System.out.println("field name-->" + field.getName());
+    
+                if (field.isAnnotationPresent(EmptyField.class)) {
+                    try {
+                        Object value = field.get(object);
+                        if (value == null || value.equals("")) {
+                            EmptyField annotation = field.getAnnotation(EmptyField.class);
                         errorMessages = br.append(annotation.message()).append("\n").toString();
+                        }
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException("Error accessing field");
                     }
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException("Error accessing field");
+                }
+    
+                if (field.isAnnotationPresent(NotValidAge.class)) {
+                    try {
+                        Number value = (Number) field.get(object);
+                        if (value.doubleValue() < 0 || value.doubleValue() > 30) {
+                            NotValidAge annotation = field.getAnnotation(NotValidAge.class);
+                        errorMessages = br.append(annotation.message()).append("\n").toString();
+                        }
+    
+                    }
+    
+                    catch (IllegalAccessException e) {
+                        throw new RuntimeException("Error accessing field");
+                    }
                 }
             }
-
-            if (field.isAnnotationPresent(NotValidAge.class)) {
-                try {
-                    Number value = (Number) field.get(object);
-                    if (value.doubleValue() < 0 || value.doubleValue() > 30) {
-                        NotValidAge annotation = field.getAnnotation(NotValidAge.class);
-                        errorMessages = br.append(annotation.message()).append("\n").toString();
-                    }
-
-                }
-
-                catch (IllegalAccessException e) {
-                    throw new RuntimeException("Error accessing field");
-                }
-            }
+            clazz = clazz.getSuperclass();
         }
+
 
         if (errorMessages != null) {
             System.out.println(errorMessages);
-            throw new RuntimeException(errorMessages);
+            throw new RuntimeException(errorMessages);    
         }
-
     }
 }
